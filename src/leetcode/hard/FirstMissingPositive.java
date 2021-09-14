6b408c99f3e980d79cf1e80be669701fe75a412a
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2021-09-14 23:23:58 +0400

    Efficient solution for returning smallest missing positive integer

--- src/leetcode/hard/FirstMissingPositive.java
+++ src/leetcode/hard/FirstMissingPositive.java
@@ -1,12 +1,18 @@
 package leetcode.hard;
 
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class FirstMissingPositive {
     public static void main(String[] args) {
-        System.out.println(firstMissingPositive(new int[] {1, 2, 0}) == 3);
-        System.out.println(firstMissingPositive(new int[] {3, 4, -1, 1}) == 2);
-        System.out.println(firstMissingPositive(new int[] {7, 8, 9, 11, 12}) == 1);
+        System.out.println(solutionOne(new int[] {1, 2, 0}) == 3);
+        System.out.println(solutionOne(new int[] {3, 4, -1, 1}) == 2);
+        System.out.println(solutionOne(new int[] {7, 8, 9, 11, 12}) == 1);
+        // -------------------------------------------------------------------------------------------------------------
+        System.out.println(solutionTwo(new int[] {1, 2, 0}) == 3);
+        System.out.println(solutionTwo(new int[] {3, 4, -1, 1}) == 2);
+        System.out.println(solutionTwo(new int[] {7, 8, 9, 11, 12}) == 1);
     }
 
     /**
@@ -14,7 +20,7 @@ public class FirstMissingPositive {
      *
      * @return the smallest missing positive integer.
      */
-    public static int firstMissingPositive(int[] numbers) {
+    public static int solutionOne(int[] numbers) {
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
         for (int num: numbers) {
@@ -32,5 +38,28 @@ public class FirstMissingPositive {
         }
 
         return result;
+    }
+
+    /**
+     * @param numbers array of numbers.
+     *
+     * @return the smallest missing positive integer.
+     */
+    public static int solutionTwo(int[] numbers) {
+        Set<Integer> mySet = new HashSet<>();
+
+        for (int num: numbers) {
+            if (num > 0 && num < numbers.length + 1) {
+                mySet.add(num);
+            }
+        }
+
+        for (int i = 1; i <= numbers.length; ++i) {
+            if (!mySet.contains(i)) {
+                return i;
+            }
+        }
+
+        return numbers.length + 1;
     }
 }
